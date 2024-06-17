cd63cf45083b39be9bd55a4c85602208d1fa1e1c
Alex Straton <alexandrastraton1@gmail.com>
2024-06-17 10:57:27 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const {
   getAllTopics,
   getAllEndpoints,
@@ -21,6 +23,8 @@ const {
 } = require("./controller/users.controller");
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/api/topics", getAllTopics); //y
